cc0647875cc05d206fe3a14b597d994374118999
Mahesh Raj <maheshraj6761@gmail.com>
2025-12-11 19:43:14 +0530

    changes

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from task import ThreadPool
 from ui import ThreadUI
 
 if __name__ == "__main__":
-    print("Logging system initialized")  # <--- Add this line
+    print("Logging system initialized")  
     pool = ThreadPool(num_threads=1)
     ThreadUI(pool)
